1c67226110d3eff298f121e4b4d11613d44fbe89
Odunosh Moses <Odunoshomoses@gmail.com>
2022-10-18 20:28:22 +0000

    added ant library and firebase setup

---
+++ linktree/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBzAyDspfJo1-5V8haWEplExzwQpFiJEXM",
+    authDomain: "dsa-forum.firebaseapp.com",
+    projectId: "dsa-forum",
+    storageBucket: "dsa-forum.appspot.com",
+    messagingSenderId: "678131605851",
+    appId: "1:678131605851:web:78f0eabc7c245a14063a7e",
+    databaseURL: "https://dsa-forum-default-rtdb.firebaseio.com/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
